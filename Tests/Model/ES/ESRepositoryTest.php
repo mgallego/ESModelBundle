9ed752e081b85fc780c49dd205b48bc08673cd0c
moisesgallego <moisesgallego@gmail.com>
2014-03-27 22:28:22 +0100

    Test to Test the Tests

---
+++ Tests/Model/ES/ESRepositoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MGP\ESModelBundle\Tests\Model\ES\Repository;
+
+class ESRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
